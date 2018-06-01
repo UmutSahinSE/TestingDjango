6766dba1e87b712f6b4521d6c1c8a7891d706f45
Umut Şahin <umut.sahin.se@gmail.com>
2018-06-01 11:13:22 +0300

    model&migration&database

--- myProfile/models.py
+++ myProfile/models.py
@@ -1,6 +1,10 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class ProgrammingLanguages(models.Model):
+    langName = models.CharField(max_length=25)
+    proficiency = models.PositiveSmallIntegerField()
+    years = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.langName
