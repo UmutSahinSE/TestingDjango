854b8f1bb775c11f34e4db6fb265b2f937a1975d
Umut Şahin <umut.sahin.se@gmail.com>
2018-06-05 19:47:46 +0300

    accessing entries from database

--- myProfile/views.py
+++ myProfile/views.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from .models import ProgrammingLanguages
 
 def showProfile(request):
-    return render(request,'myProfile/profilePage.html')
+    langObjects=ProgrammingLanguages.objects.all().order_by('proficiency')
+    return render(request,'myProfile/profilePage.html',{'langObjects':langObjects})
