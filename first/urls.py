7fc769b26d7d99967b2c84463694d9274960a225
Umut Şahin <umut.sahin.se@gmail.com>
2018-06-07 15:39:12 +0300

    link to profile page

--- first/urls.py
+++ first/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url,include
 from django.contrib import admin
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^myProfile/',include('myProfile.urls')),
     url(r'^$', views.showHome),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- myProfile/urls.py
+++ myProfile/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.showProfile),
+    url(r'^$', views.showProfile,name="profile"),
 ]
